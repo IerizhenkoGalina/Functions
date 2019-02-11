9e6b41504d6c6c387ccdab0b21da30953dbb058b
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 09:01:49 +0200

    Create Default parameter.js
    By using a default parameter, we account for situations when an argument isn't passed into a function that is expecting an argument.

---
+++ Default parameter.js
@@ -0,0 +1,9 @@
+function makeShoppingList(item = "milk") {
+  console.log(`Remember to buy ${item}.`);
+  }
+makeShoppingList();
+makeShoppingList('bread');
+makeShoppingList('eggs');
+//Prints Remember to buy milk.
+//Prints Remember to buy bread.
+//Prints Remember to buy eggs.
